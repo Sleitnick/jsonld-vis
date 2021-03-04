1a6b46f95ddd75384a43aa99ca7829abfb886135
Stephen Leitnick <sleitnick@gmail.com>
2021-03-04 15:36:29 -0500

    Upgrade

--- example/index.js
+++ example/index.js
@@ -1,12 +1,14 @@
-import d3 from 'd3';
-import jsonldVis from '../src/';
+import * as d3 from 'd3';
+import jsonldVis from '../src_new/';
 import data from './example.json';
 
 jsonldVis(d3);
 
 d3.jsonldVis(data, '#graph', {
-  w: 800,
-  h: 600,
-  maxLabelWidth: 250,
-  tipClassName: 'tip-class'
+	w: 800,
+//	h: 600,
+	maxLabelWidth: 250,
+	tipClassName: 'tip-class',
+	transitionDuration: 250,
+	transitionEase: 'cubic-out'
 });
